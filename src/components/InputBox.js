47feab36d512542df5531e054333ba1ded1c57bd
rochafialvin <rochafi.dev@gmail.com>
2022-06-07 11:39:47 +0700

    State

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
+
 function InputBox() {
+  // keyword akan menjadi variable yg menyimpan data
+  // setKeyword adalah function untuk update keyword
+  const [keyword, setKeyword] = useState(100);
+
+  const onAddHandler = () => {
+    console.log(keyword);
+  };
+
+  const onInputChange = (event) => {
+    // Value pada tag input akan disimpan ke state keyword
+    setKeyword(event.target.value);
+  };
+
   return (
     <div>
-      <p>Done 1 of 3</p>
+      <p>Isi dari keyword : {keyword}</p>
       <input
         type="text"
         className="form-control"
         placeholder="What is next ?"
+        onChange={onInputChange}
       />
-      <button className="btn btn-outline-primary w-100 mt-2">Add</button>
+      <button
+        onClick={onAddHandler}
+        className="btn btn-outline-primary w-100 mt-2"
+      >
+        Add
+      </button>
     </div>
   );
 }
